63d6ca4dec185683fb926bf530a7147b9e987291
Michael Evangelos Diamantis <13-13246@saeinstitute.edu>
2022-04-15 14:25:34 +0300

    Project update
    Added gun funtions.
    Added particle and trail effects.

---
+++ shootingDoneRight/Assets/_Scripts/CrosshairTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairTarget : MonoBehaviour
+{
+    Camera mainCamera;
+
+    Ray ray;
+    RaycastHit hitInfo;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        ray.origin = mainCamera.transform.position;
+        ray.direction = mainCamera.transform.forward;
+
+        Physics.Raycast(ray, out hitInfo);
+
+        //Works like Doom 3 / Cyberpunk panel interaction
+        transform.position = hitInfo.point;
+    }
+}
---
+++ shootingDoneRight/Assets/_Scripts/Weapon.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] bool isFiring = false;
+    [SerializeField] Transform raycastOrigin;
+    [SerializeField] Transform raycastDestination;
+    [SerializeField] TrailRenderer tracerEffect;
+    [SerializeField] ParticleSystem hitEffect;
+
+    Ray ray;
+    RaycastHit hitInfo;
+
+    private void Awake()
+    {
+        SetDefaults();
+    }
+
+    void SetDefaults()
+    {
+        isFiring = false;
+    }
+
+    void StartFiring()
+    {
+        isFiring = true;
+
+        ray.origin = raycastOrigin.position;
+        ray.direction = raycastDestination.position - raycastOrigin.position;
+
+        TrailRenderer tracer = Instantiate(tracerEffect, ray.origin, Quaternion.identity);
+        tracer.AddPosition(ray.origin);
+
+        if(Physics.Raycast(ray, out hitInfo))
+        {
+            hitEffect.transform.position = hitInfo.point;
+            hitEffect.transform.forward = hitInfo.normal;
+
+            hitEffect.Emit(1);
+
+            tracer.transform.position = hitInfo.point;
+        }
+    }
+
+    void StopFiring()
+    {
+        isFiring = false;
+    }
+
+    private void LateUpdate()
+    {
+        //Separate inputs in actual game
+        if(Input.GetButtonDown("Fire1"))
+        {
+            StartFiring();
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopFiring();
+        }
+    }
+}
